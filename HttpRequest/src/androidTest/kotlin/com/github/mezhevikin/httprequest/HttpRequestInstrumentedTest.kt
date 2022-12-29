edb6963857e0d6414685fb9d6a2469f60b84d164
mezhevikin <mezhevikin@gmail.com>
2022-12-29 15:31:57 +0400

    Add HttpRequest

---
+++ HttpRequest/src/androidTest/kotlin/com/github/mezhevikin/httprequest/HttpRequestInstrumentedTest.kt
@@ -0,0 +1,131 @@
+package com.github.mezhevikin.httprequest
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Assert.*
+import java.net.SocketTimeoutException
+import java.util.concurrent.CountDownLatch
+
+@RunWith(AndroidJUnit4::class)
+class HttpRequestInstrumentedTest {
+    @Test fun testGetRequest() {
+        val latch = CountDownLatch(1)
+        val request = HttpRequest(
+            url = "https://httpbin.org/get",
+            method = Method.GET,
+            parameters = mapOf(
+                "firstName" to "Alexey",
+                "lastName" to "Mezhevikin"
+            ),
+            headers = mapOf(
+                "User-Agent" to "HttpRequest"
+            )
+        )
+        request.json<HttpBin> { json, response ->
+            assertNotNull(response.body)
+            assertNull(response.exception)
+            assertNotNull(json)
+            assertEquals(200, response.connection?.responseCode)
+            assertTrue(response.success)
+            assertEquals(json?.args?.get("firstName"), "Alexey")
+            assertEquals(json?.args?.get("lastName"), "Mezhevikin")
+            assertEquals(json?.headers?.get("User-Agent"), "HttpRequest")
+            latch.countDown()
+        }
+        latch.await()
+    }
+
+    @Test fun testPostRequest() {
+        val latch = CountDownLatch(1)
+        val request = HttpRequest(
+            url = "https://httpbin.org/post",
+            method = Method.POST,
+            parameters = mapOf(
+                "firstName" to "Alexey",
+                "lastName" to "Mezhevikin"
+            ),
+            headers = mapOf(
+                "User-Agent" to "HttpRequest"
+            )
+        )
+        request.json<HttpBin> { json, response ->
+            assertNotNull(response.body)
+            assertNull(response.exception)
+            assertNotNull(json)
+            assertEquals(200, response.connection?.responseCode)
+            assertTrue(response.success)
+            assertEquals(json?.form?.get("firstName"), "Alexey")
+            assertEquals(json?.form?.get("lastName"), "Mezhevikin")
+            assertEquals(json?.headers?.get("User-Agent"), "HttpRequest")
+            latch.countDown()
+        }
+        latch.await()
+    }
+
+    @Test fun testHeadRequest() {
+        val latch = CountDownLatch(1)
+        HttpRequest("https://httpbin.org/headers", Method.HEAD).response { response ->
+            assertEquals(200, response.connection?.responseCode)
+            assertEquals(response.body, "")
+            assertNull(response.exception)
+            latch.countDown()
+        }
+        latch.await()
+    }
+
+    @Test fun testNotFound() {
+        val latch = CountDownLatch(1)
+        HttpRequest("https://httpbin.org/status/404").response { response ->
+            assertFalse(response.success)
+            assertNotNull(response.exception)
+            latch.countDown()
+        }
+        latch.await()
+    }
+
+    @Test fun testRequestTimeout() {
+        val latch = CountDownLatch(1)
+        val request = HttpRequest("https://httpbin.org/delay/10", config = {
+            it.readTimeout = 1000
+        })
+        request.response { response ->
+            assertNull(response.body)
+            assertNotNull(response.exception)
+            assertTrue(response.exception is SocketTimeoutException)
+            latch.countDown()
+        }
+        latch.await()
+    }
+
+    @Test fun testJsonError() {
+        val latch = CountDownLatch(1)
+        val request = HttpRequest("https://httpbin.org/get")
+        request.json = Json { ignoreUnknownKeys = false }
+        request.json<HttpBin> { json, response ->
+            assertNotNull(response.exception)
+            assertNull(json)
+            latch.countDown()
+        }
+        latch.await()
+    }
+
+    @Test fun testQueryEncoding() {
+        assertEquals(
+            "username=mezhevikin&password=%21%40%23%24%25%5E%26*%28%29",
+            mapOf(
+                "username" to "mezhevikin",
+                "password" to "!@#$%^&*()"
+            ).query
+        )
+    }
+}
+
+@Serializable
+data class HttpBin(
+    val args: Map<String, String>? = null,
+    val form: Map<String, String>? = null,
+    val headers: Map<String, String>? = null
+)
---
+++ HttpRequest/src/main/kotlin/com/github/mezhevikin/httprequest/HttpRequest.kt
@@ -0,0 +1,89 @@
+package com.github.mezhevikin.httprequest
+
+import java.net.HttpURLConnection
+import java.net.URLEncoder
+import java.net.URL
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.concurrent.Executors
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.decodeFromString
+
+class HttpRequest(
+    val url: String,
+    val method: Method = Method.GET,
+    val parameters: Map<String, Any> = mapOf(),
+    val headers: Map<String, String> = mapOf(),
+    val config: ((HttpURLConnection) -> Unit)? = null
+) {
+    fun response(completion: (HttpResponse) -> Unit) {
+        Executors.newSingleThreadExecutor().execute {
+            try {
+                val url = if (method == Method.GET && parameters.isNotEmpty()) {
+                    URL("$url?${parameters.query}")
+                } else {
+                    URL(url)
+                }
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = method.value
+                headers.forEach { (key, value) ->
+                    connection.setRequestProperty(key, value)
+                }
+                config?.let { it(connection) }
+                if (method == Method.POST && parameters.isNotEmpty()) {
+                    connection.doOutput = true
+                    connection.outputStream.use {
+                        it.write(parameters.query.toByteArray())
+                    }
+                }
+                val response = HttpResponse()
+                response.connection = connection
+                response.body = connection.inputStream.use {
+                    BufferedReader(InputStreamReader(it)).use { reader ->
+                        reader.readText()
+                    }
+                }
+                completion(response)
+            } catch (e: Exception) {
+                val response = HttpResponse()
+                response.exception = e
+                completion(response)
+            }
+        }
+    }
+
+    var json = Json { ignoreUnknownKeys = true }
+
+    inline fun <reified T> json(crossinline completion: (T?, HttpResponse) -> Unit) where T : Any {
+        response { response ->
+            try {
+                val body = response.body
+                val result = if (body != null) json.decodeFromString<T>(body) else null
+                completion(result, response)
+            } catch (e: Exception) {
+                response.exception = e
+                completion(null, response)
+            }
+        }
+    }
+}
+
+enum class Method(val value: String) {
+    GET("GET"), POST("POST"), HEAD("HEAD"), PUT("PUT"), DELETE("DELETE")
+}
+
+class HttpResponse {
+    var connection: HttpURLConnection? = null
+    var body: String? = null
+    var exception: Exception? = null
+    val success: Boolean get() {
+        connection?.let { return it.responseCode in 200..299 }
+        return false
+    }
+}
+
+val Map<String, Any>.query: String get() {
+    return this.map { (key, value) -> "$key=${value.urlEncoded}" }.joinToString("&")
+}
+
+val Any.urlEncoded: String get() = URLEncoder.encode(toString(), "UTF-8")
